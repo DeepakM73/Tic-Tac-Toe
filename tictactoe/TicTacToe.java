708c21ddaeeb1fa6ffd5aeb8032fdb190f8af87b
DeepakM73 <deepak.muddasani73@gmail.com>
2021-09-11 08:57:43 +0530

    Added: Board

--- tictactoe/TicTacToe.java
+++ tictactoe/TicTacToe.java
@@ -7,7 +7,7 @@ public class TicTacToe {
     public static void board() {
         char[] emptyBoard = new char[10];
         for (int i = 0; i < 10 ; i++) {
-            emptyBoard[i] = ' ';
+            emptyBoard[i] = ' ' ;
         }
     }
 }
