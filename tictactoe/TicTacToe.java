bda3ab77cb7a19fe1b33064a1d6753cb6804b294
DeepakM73 <deepak.muddasani73@gmail.com>
2021-09-11 08:56:35 +0530

    Tic Tac Toe

---
+++ tictactoe/TicTacToe.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.tictactoe;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+    }
+    public static void board() {
+        char[] emptyBoard = new char[10];
+        for (int i = 0; i < 10 ; i++) {
+            emptyBoard[i] = ' ';
+        }
+    }
+}
