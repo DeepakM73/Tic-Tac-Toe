f2bf7b2f175b1fb21945215650a038919a63aa24
DeepakM73 <deepak.muddasani73@gmail.com>
2021-09-11 09:45:19 +0530

    Added: Choosing Options X OR O

--- tictactoe/TicTacToe.java
+++ tictactoe/TicTacToe.java
@@ -1,13 +1,33 @@
 package com.bridgelabz.tictactoe;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class TicTacToe {
+    public static char playerChoice;
+    public static char computerChoice;
 
     public static void main(String[] args) {
+
     }
     public static void board() {
         char[] emptyBoard = new char[10];
         for (int i = 0; i < 10 ; i++) {
             emptyBoard[i] = ' ' ;
+        }
+    }
+
+    public static void chooseXorO(Scanner scanner) {
+        System.out.println("Which one you want ?\n" +"'X' or 'O'");
+        Scanner sc = new Scanner(System.in);
+        char playerChoice = sc.next().charAt(0);
+        if(playerChoice=='X') {
+            computerChoice = 'O';
+            System.out.println("Comp enters"+ computerChoice);
+        }
+        else {
+            computerChoice = 'X';
+            System.out.println("Comp enters"+ computerChoice);
         }
     }
 }
